ed27cc31e8c599a74464400834b649d1e1b1f268
Ulises Cadena <54476857+ucadena07@users.noreply.github.com>
2020-07-29 18:54:57 -0500

    update

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -37,3 +37,29 @@ function loadDataTable() {
         "width": "100%"
     });
 }
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.success(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
